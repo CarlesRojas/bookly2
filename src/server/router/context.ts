067243cddb66866eea28266c23dfed6c959d7810
CarlesRojas <carles.rojas@mundimoto.com>
2022-08-28 11:53:28 +0200

    update import paths

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -2,8 +2,8 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { Session, unstable_getServerSession as getServerSession } from "next-auth";
-import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
-import { prisma } from "../db/client";
+import { authOptions as nextAuthOptions } from "@pages/api/auth/[...nextauth]";
+import { prisma } from "@server/db/client";
 
 type CreateContextOptions = {
     session: Session | null;
