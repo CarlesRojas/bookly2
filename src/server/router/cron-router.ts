fc8dcd42467b4037f4dd903e189cb37485f150d5
CarlesRojas <carles.rojas@mundimoto.com>
2022-09-07 09:44:59 +0200

    return something from cron

--- src/server/router/cron-router.ts
+++ src/server/router/cron-router.ts
@@ -3,5 +3,7 @@ import { createRouter } from "./context";
 export const cronRouter = createRouter().query("get-all", {
     async resolve() {
         console.log("hola");
+
+        return { success: true };
     },
 });
